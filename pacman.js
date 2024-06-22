6e8d848dd27333563f18fb3fef41978114e1ec40
Naga Chaitanya Varma <69190057+Chay2203@users.noreply.github.com>
2024-06-22 22:41:27 +0530

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -11,7 +11,8 @@ let pacMan = {
     dx: tileSize,
     dy: 0,
     radius: tileSize / 2,
-    angle: 0.3
+    angle: 0.3,
+    direction: 'right'
 };
 
 const walls = [
@@ -21,11 +22,20 @@ const walls = [
     // Add more walls as needed
 ];
 
+let pellets = [];
+for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+        if (!walls.some(wall => wall.x === i && wall.y === j)) {
+            pellets.push({x: i, y: j});
+        }
+    }
+}
+
 document.addEventListener('keydown', changeDirection);
 
 function drawPacMan() {
     ctx.beginPath();
-    ctx.arc(pacMan.x + pacMan.radius, pacMan.y + pacMan.radius, pacMan.radius, pacMan.angle * Math.PI, (2 - pacMan.angle) * Math.PI);
+    ctx.arc(pacMan.x + pacMan.radius, pacMan.y + pacMan.radius, pacMan.radius, (pacMan.angle + pacMan.directionAngle) * Math.PI, (2 - pacMan.angle + pacMan.directionAngle) * Math.PI);
     ctx.lineTo(pacMan.x + pacMan.radius, pacMan.y + pacMan.radius);
     ctx.fillStyle = 'yellow';
     ctx.fill();
@@ -39,46 +49,104 @@ function drawWalls() {
     }
 }
 
+function drawPellets() {
+    ctx.fillStyle = 'white';
+    for (const pellet of pellets) {
+        ctx.beginPath();
+        ctx.arc(pellet.x * tileSize + tileSize / 2, pellet.y * tileSize + tileSize / 2, tileSize / 5, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
 function changeDirection(event) {
     const keyPressed = event.keyCode;
+    let newDirection;
 
     switch(keyPressed) {
         case 37: // left arrow
-            pacMan.dx = -tileSize;
-            pacMan.dy = 0;
+            newDirection = 'left';
             break;
         case 38: // up arrow
-            pacMan.dx = 0;
-            pacMan.dy = -tileSize;
+            newDirection = 'up';
             break;
         case 39: // right arrow
-            pacMan.dx = tileSize;
-            pacMan.dy = 0;
+            newDirection = 'right';
             break;
         case 40: // down arrow
-            pacMan.dx = 0;
-            pacMan.dy = tileSize;
+            newDirection = 'down';
             break;
+    }
+
+    if (newDirection && !detectWallCollision(newDirection)) {
+        pacMan.direction = newDirection;
+        switch (newDirection) {
+            case 'left':
+                pacMan.dx = -tileSize;
+                pacMan.dy = 0;
+                pacMan.directionAngle = 1;
+                break;
+            case 'up':
+                pacMan.dx = 0;
+                pacMan.dy = -tileSize;
+                pacMan.directionAngle = 1.5;
+                break;
+            case 'right':
+                pacMan.dx = tileSize;
+                pacMan.dy = 0;
+                pacMan.directionAngle = 0;
+                break;
+            case 'down':
+                pacMan.dx = 0;
+                pacMan.dy = tileSize;
+                pacMan.directionAngle = 0.5;
+                break;
+        }
     }
 }
 
 function movePacMan() {
-    pacMan.x += pacMan.dx;
-    pacMan.y += pacMan.dy;
+    if (!detectWallCollision(pacMan.direction)) {
+        pacMan.x += pacMan.dx;
+        pacMan.y += pacMan.dy;
+    }
 
     if (pacMan.x < 0) pacMan.x = canvas.width - tileSize;
     else if (pacMan.x >= canvas.width) pacMan.x = 0;
     if (pacMan.y < 0) pacMan.y = canvas.height - tileSize;
     else if (pacMan.y >= canvas.height) pacMan.y = 0;
+
+    detectPelletCollision();
 }
 
-function detectCollision() {
-    for (const wall of walls) {
-        if (pacMan.x === wall.x * tileSize && pacMan.y === wall.y * tileSize) {
-            pacMan.dx = 0;
-            pacMan.dy = 0;
-        }
+function detectWallCollision(direction) {
+    let nextX = pacMan.x;
+    let nextY = pacMan.y;
+
+    switch (direction) {
+        case 'left':
+            nextX -= tileSize;
+            break;
+        case 'up':
+            nextY -= tileSize;
+            break;
+        case 'right':
+            nextX += tileSize;
+            break;
+        case 'down':
+            nextY += tileSize;
+            break;
     }
+
+    return walls.some(wall => wall.x * tileSize === nextX && wall.y * tileSize === nextY);
+}
+
+function detectPelletCollision() {
+    pellets = pellets.filter(pellet => {
+        if (pacMan.x === pellet.x * tileSize && pacMan.y === pellet.y * tileSize) {
+            return false;
+        }
+        return true;
+    });
 }
 
 function clearCanvas() {
@@ -88,9 +156,9 @@ function clearCanvas() {
 function gameLoop() {
     clearCanvas();
     drawWalls();
+    drawPellets();
     drawPacMan();
     movePacMan();
-    detectCollision();
     requestAnimationFrame(gameLoop);
 }
 
